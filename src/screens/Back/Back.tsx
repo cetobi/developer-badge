44f155cd3a31c4afd1d8e4a7a199425585dcd17e
cetobi <andre.marcdias@gmail.com>
2024-01-14 13:45:10 -0300

    feat: base Front and Back screens

---
+++ src/screens/Back/Back.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { styles } from './BackStyle';
+
+export function Back() {
+  return (
+    <View style={styles.container}>
+        <Text>Back</Text>
+    </View>
+  );
+}
---
+++ src/screens/Front/Front.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { styles } from './FrontStyle';
+
+export function Front() {
+  return (
+    <View style={styles.container}>
+        <Text>Front</Text>
+    </View>
+  );
+}
