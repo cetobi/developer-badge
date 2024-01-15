58a149f4182cc1361233067b0d00a253773999c1
cetobi <andre.marcdias@gmail.com>
2024-01-14 22:52:30 -0300

    feat: added icons tab bar

--- src/routes/bottomRoutes.tsx
+++ src/routes/bottomRoutes.tsx
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { AntDesign } from '@expo/vector-icons';
 
 import { Front } from '../screens/Front/Front'
 import { Back } from '../screens/Back/Back'
@@ -8,8 +9,19 @@ const { Screen, Navigator } = createBottomTabNavigator()
 export function BottomRoutes() {
     return (
         <Navigator>
-            <Screen name="Front" component={Front} />
-            <Screen name="Back" component={Back} />
+            <Screen name="Front" component={Front}
+                options={{
+                    tabBarIcon: () => (
+                        <AntDesign name="user" size={20} color="black" />
+                    ),
+                }}
+            />
+            <Screen name="Back" component={Back}
+                options={{
+                    tabBarIcon: () => (
+                        <AntDesign name="idcard" size={24} color="black" />),
+                }}
+            />
         </Navigator>
     )
 }
