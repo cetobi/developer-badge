ae945bd5b0d20bcb5c61930b375616fe3939f664
cetobi <andre.marcdias@gmail.com>
2024-01-16 20:33:29 -0300

    feat: created card component for technologies

---
+++ src/components/TechCard/TechCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+import { styles } from './TechCardStyle';
+
+export function TechCard(prop:any) {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.textName}>
+            {prop.language !== undefined ? String(prop.language) : null}:
+        </Text>
+        <Text style={styles.textValue}>
+            {prop.value !== undefined ? String(prop.value) : null}%
+        </Text>
+    </View>
+  );
+}
---
+++ src/components/TechCard/TechCardStyle.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent:'space-around',
+    alignItems: 'center',
+    marginVertical: 3,
+  },
+  textName: {
+    fontWeight: 'bold',
+    marginRight: 10,
+  },
+  textValue: {
+
+  }
+});
