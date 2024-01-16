e08d8872bfbfeb7429126232f7e85db3c450d928
cetobi <andre.marcdias@gmail.com>
2024-01-16 20:53:28 -0300

    feat: adjust to values equal to 0

--- src/components/TechCard/TechCard.tsx
+++ src/components/TechCard/TechCard.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 import { styles } from './TechCardStyle';
 
-export function TechCard(prop:any) {
-  return (
-    <View style={styles.container}>
-        <Text style={styles.textName}>
-            {prop.language !== undefined ? String(prop.language) : null}:
-        </Text>
-        <Text style={styles.textValue}>
-            {prop.value !== undefined ? String(prop.value) : null}%
-        </Text>
-    </View>
-  );
+export function TechCard(prop: any) {
+    return (
+        (prop.value > 0 && (
+            <View style={styles.container}>
+                <Text style={styles.textName}>
+                    {prop.language !== undefined ? String(prop.language) : null}:
+                </Text>
+                <Text style={styles.textValue}>
+                    {prop.value !== undefined ? String(prop.value) : null}%
+                </Text>
+            </View>
+        ))
+    );
 }
