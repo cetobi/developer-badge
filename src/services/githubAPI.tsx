35a43aa4cde6054a2d02e72b5019535be0a32009
cetobi <andre.marcdias@gmail.com>
2024-01-16 19:58:23 -0300

    feat: added search for repositories and search for languages

--- src/services/githubAPI.tsx
+++ src/services/githubAPI.tsx
@@ -9,3 +9,23 @@ export const getUser = async (nickname: string) => {
         return null;
     }
 }
+
+export const getReposUser = async (repos_url: string) => {
+    try {
+        const response: any = await axios.get(repos_url);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+export const getLanguageReposUser = async (nickname: string, nameRepos: string) => {
+    try {
+        const response: any = await axios.get(`https://api.github.com/repos/${nickname}/${nameRepos}/languages`);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
